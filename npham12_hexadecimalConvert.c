d0f74066237ded5a30d5e4c729edc05ba7030f41
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-22 14:34:36 -0800

    Update npham12_hexadecimalConvert.c

--- npham12_hexadecimalConvert.c
+++ npham12_hexadecimalConvert.c
@@ -1,10 +1,3 @@
-/* Hexadecimal Convert
-   Billy Pham*/
-
-/*The program will ask the user for an integer and then display the hex representation of the number
- Using bit shifting and masking operations
- Prompt the user for an integer number and print out its hex representation*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
